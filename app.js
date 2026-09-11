94241efe37bf9cc6094689923260b5c4c5774f19
laumannaira-bit <laumannaira@gmail.com>
2026-09-10 21:05:23 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+javascript
+
+document.addEventListener('DOMContent const crearSalaBtn = document.getElem const unirseSalaBtn = document.getElei
+
+crearSalaBtn.addEventListener('click', () console.log('Crear Sala clicked'); });
+
+unirseSalaBtn.addEventListener('click', ( console.log('Unirse Sala clicked'); }); });
